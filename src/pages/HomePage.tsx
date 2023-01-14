c78a9bafaad89d3636adec0200180017a15bd947
igor <igorbelikov2009@yandex.ru>
2023-01-14 13:31:24 +0300

    getuserrepos

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,73 @@
-import React, { FC } from "react";
-import { useSearchUserQuery } from "../store/github/github.api";
+import React, { FC, useEffect, useState } from "react";
+import { useDebounce } from "../hooks/debounce";
+import { useLazyGetUserReposQuery, useSearchUserQuery } from "../store/github/github.api";
 
 const HomePage: FC = () => {
-  const { data, isLoading, isError } = useSearchUserQuery("vladilen");
+  const [search, setSearch] = useState("");
+  const [dropdown, setDropdown] = useState(false);
 
-  return <div>HomePage</div>;
+  // хуком useDebounce() мы оптимизировали набор символов в нашем инпуте
+  const debounced = useDebounce(search);
+  const { data, isLoading, isError } = useSearchUserQuery(debounced, {
+    skip: debounced.length < 3,
+    // Очень интересная штука, для обновления страницы через автоматический запрос.
+    // Как только, окно инпута получает фокус, после какого-то перерыва в работе,
+    // запрос осуществляется автоматически, страничка обновляется и становится
+    // актуальной на текущий момент времени.
+    // Предварительно настраиваем в githubApi стр.9 и store/index.js стр.13
+    refetchOnFocus: true,
+  });
+
+  const [fetchRepos, { isLoading: isLoadingRepos, data: repos }] = useLazyGetUserReposQuery();
+
+  useEffect(() => {
+    // console.log(debounced);
+    setDropdown(debounced.length > 3 && data?.length! > 0);
+  }, [data?.length, debounced]);
+
+  const clickHandler = (username: string) => {
+    // console.log(username);
+    fetchRepos(username);
+  };
+
+  return (
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      {isError && <p className="text-center text-red-600">Error...</p>}
+
+      <div className="relative w-[560px]">
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          className="border py-2 px-4 w-full h-[42px] mb-2"
+          placeholder="Search for Github username..."
+        />
+
+        {dropdown && (
+          <ul className="list-none absolute top-[42px]  left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white">
+            {isLoading && <p className="text-center">Loading...</p>}
+            {data &&
+              data.map((user) => (
+                <li
+                  key={user.id}
+                  onClick={() => clickHandler(user.login)}
+                  className={"py-2 hover: bg-gray-500 hover:text-white transition-colors cursor-pointer"}
+                >
+                  {user.login}
+                </li>
+              ))}
+          </ul>
+        )}
+
+        <div className="container">
+          {isLoadingRepos && <p className="text-center"> Repos loading...</p>}
+          {repos?.map((repo) => (
+            <p key={repo.id}> {repo.url} </p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
--- src/store/github/github.api.ts
+++ src/store/github/github.api.ts
@@ -1,3 +1,4 @@
+import { IRepo, IUser, ServerResponse } from "./../../models/models";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const githubApi = createApi({
@@ -5,16 +6,27 @@ export const githubApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://api.github.com/",
   }),
+  refetchOnFocus: true,
   endpoints: (build) => ({
-    searchUser: build.query<any, string>({
+    searchUser: build.query<IUser[], string>({
       query: (search: string) => ({
         url: `search/users`,
         params: {
           q: search,
+          // per_page - параметр количества элементов для страницы, прилетающих с сервера.
+          per_page: 10,
         },
+      }),
+      transformResponse: (response: ServerResponse<IUser>) => response.items,
+    }),
+    getUserRepos: build.query<IRepo[], string>({
+      query: (username: string) => ({
+        url: `users/${username}/repos`,
       }),
     }),
   }),
 });
 
-export const { useSearchUserQuery } = githubApi;
+export const { useSearchUserQuery, useLazyGetUserReposQuery } = githubApi;
+// useGetUserReposQuery или useLazyGetUserReposQuery? Lazy говорит нам о том, что мы можем
+// сделать запрос, когда захотим.
